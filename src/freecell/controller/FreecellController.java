0a44ba91099216b0d8269ba335fa1ee5f60ecdcb
yash-bhavsar <yashbhavsar5895@gmail.com>
2018-11-06 18:11:54 -0500

    updated code

--- src/freecell/controller/FreecellController.java
+++ src/freecell/controller/FreecellController.java
@@ -1,7 +1,6 @@
 package freecell.controller;
 
 import java.io.IOException;
-import java.nio.channels.IllegalSelectorException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,8 +13,8 @@ import freecell.model.PileType;
 public class FreecellController implements IFreecellController {
 
 
-  private  Readable rd;
-  private  Appendable ap;
+  private Readable rd;
+  private Appendable ap;
   private PileType sourcePileType = null;
   private PileType destPileType = null;
   private int sourcePileNumber = Integer.MAX_VALUE;
@@ -49,29 +48,36 @@ public class FreecellController implements IFreecellController {
       throw new IllegalArgumentException("model not initialized");
     }
 
-    int number;
+    try {
+      model.startGame(deck, shuffle);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Start game failed");
+    }
+
+    int number = Integer.MAX_VALUE;
 
     Scanner sc = new Scanner(this.rd);
 
-    while(sc.hasNext()) {
+    while (sc.hasNext()) {
       String newString = sc.next();
       char pileType = newString.charAt(0);
       if (Character.isLetter(pileType)) {
-        if (pileType == Character.toUpperCase('Q')) {
+        if ('Q' == Character.toUpperCase(pileType)) {
           try {
-            this.ap = this.ap.append("Game quit prematurely.");
+            this.ap = this.ap.append("\nGame quit prematurely. ");
           } catch (IOException e) {
             throw new IllegalStateException("Not valid state");
           }
-        }
-        number = getPileNumber(1, newString);
-        if (this.sourcePileType == null) {
-          this.sourcePileType = getPileType(Character.toUpperCase(pileType));
-        } else if (this.destPileType == null) {
-          this.destPileType = getPileType(Character.toUpperCase(pileType));
+        } else {
+          number = getPileNumber(1, newString, sc);
+          if (this.sourcePileType == null) {
+            this.sourcePileType = getPileType(Character.toUpperCase(pileType), sc);
+          } else if (this.destPileType == null) {
+            this.destPileType = getPileType(Character.toUpperCase(pileType), sc);
+          }
         }
       } else {
-        number = getPileNumber(0, newString);
+        number = getPileNumber(0, newString, sc);
       }
 
       if (this.sourcePileNumber == Integer.MAX_VALUE) {
@@ -86,11 +92,11 @@ public class FreecellController implements IFreecellController {
               && this.sourcePileNumber != Integer.MAX_VALUE
               && this.destPileNumber != Integer.MAX_VALUE && this.cardIndex != Integer.MAX_VALUE) {
         try {
-          model.move(this.sourcePileType, this.sourcePileNumber, this.cardIndex, this.destPileType,
-                  this.destPileNumber);
+          model.move(this.sourcePileType, (this.sourcePileNumber - 1), (this.cardIndex - 1),
+                  this.destPileType, (this.destPileNumber - 1));
         } catch (Exception e) {
           try {
-            this.ap = this.ap.append("Invalid move. Try again.");
+            this.ap = this.ap.append("Invalid move. Try again. " + e.getMessage());
           } catch (IOException e1) {
             throw new IllegalStateException("State not valid");
           }
@@ -115,32 +121,51 @@ public class FreecellController implements IFreecellController {
 
   /**
    * Private method to get the source, destination pile number and card index.
+   *
    * @param i is the first index of substring.
    * @param s is the string.
    * @return the number.
    */
-  private int getPileNumber(int i, String s) {
-    try {
-      return Integer.parseInt(s.substring(i, s.length() - 1));
-    } catch (NumberFormatException ne) {
-      return Integer.MAX_VALUE;
+  private int getPileNumber(int i, String s, Scanner sc) {
+    while (true) {
+      try {
+        return Integer.parseInt(s.substring(i));
+      } catch (Exception ne) {
+
+      }
     }
   }
 
   /**
    * Private method which returns the pile type depending on the parameter passed.
+   *
    * @param pileType is the pile type passed by user.
    * @return the pile type.
    */
-  private PileType getPileType(char pileType) {
-    if (pileType == 'O') {
-      return PileType.OPEN;
-    } else if (pileType == 'C') {
-      return PileType.CASCADE;
-    } else if (pileType == 'F') {
-      return PileType.FOUNDATION;
-    } else {
-      throw new IllegalArgumentException("pile type");
+  private PileType getPileType(char pileType, Scanner sc) {
+    while (true) {
+      if (pileType == 'O') {
+        return PileType.OPEN;
+      } else if (pileType == 'C') {
+        return PileType.CASCADE;
+      } else if (pileType == 'F') {
+        return PileType.FOUNDATION;
+      } else {
+        try {
+          this.ap = ap.append(pileType);
+        } catch (IOException e) {
+          throw new IllegalStateException("pata nai");
+        }
+        String s = sc.next();
+        pileType = s.charAt(0);
+        if (Character.toUpperCase(pileType) == 'Q') {
+          try {
+            this.ap = this.ap.append("\nGame quit prematurely.");
+          } catch (IOException e) {
+            throw new IllegalStateException("Not valid state");
+          }
+        }
+      }
     }
   }
 }
--- src/freecell/model/FreecellMultiMoveModel.java
+++ src/freecell/model/FreecellMultiMoveModel.java
@@ -1,6 +1,5 @@
 package freecell.model;
 
-import java.util.LinkedList;
 import java.util.List;
 
 public class FreecellMultiMoveModel extends FreecellModel {
@@ -61,16 +60,21 @@ public class FreecellMultiMoveModel extends FreecellModel {
             && destPileNumber < noOfCascadePiles) {
       if (cardIndex <= this.cascade.get(sourcePileNumber).size() - 1) {
         int size = this.cascade.get(sourcePileNumber).size();
-        List<Card> sublist = this.cascade.get(sourcePileNumber).subList(cardIndex, size-1);
-
-        if (checkBuild(sublist) /*&& sublist.size() <= number*/) {
+        List<Card> sublist = this.cascade.get(sourcePileNumber).subList(cardIndex, size);
+        int number = (int) ((countEmptyOpenPiles() + 1) * Math.pow(2, countEmptyCascadePiles()));
+        if (checkBuild(sublist) && sublist.size() <= number) {
           Card c = sublist.get(0);
-          Card dPLCard = this.cascade.get(destPileNumber).getLast();
-          if (((dPLCard.getNumber() - 1) == c.getNumber()) && checkAlternateSuit(dPLCard, c)) {
+          if (!this.cascade.get(destPileNumber).isEmpty()) {
+            Card dPLCard = this.cascade.get(destPileNumber).getLast();
+            if (((dPLCard.getNumber() - 1) == c.getNumber()) && checkAlternateSuit(dPLCard, c)) {
+              this.cascade.get(destPileNumber).addAll(sublist);
+              this.cascade.get(sourcePileNumber).removeAll(sublist);
+            } else {
+              throw new IllegalArgumentException("Invalid Card");
+            }
+          } else {
             this.cascade.get(destPileNumber).addAll(sublist);
             this.cascade.get(sourcePileNumber).removeAll(sublist);
-          } else {
-            throw new IllegalArgumentException("Invalid Card");
           }
         }
       } else {
@@ -81,11 +85,31 @@ public class FreecellMultiMoveModel extends FreecellModel {
     }
   }
 
+  private int countEmptyOpenPiles() {
+    int noOfEmptyOpenPiles = 0;
+    for (int i = 0; i < this.noOfOpenPiles; i++) {
+      if (this.open.get(i) == null) {
+        noOfEmptyOpenPiles += 1;
+      }
+    }
+    return noOfEmptyOpenPiles;
+  }
+
+  private int countEmptyCascadePiles() {
+    int noOfEmptyCascadePiles = 0;
+    for (int i = 0; i < this.noOfCascadePiles; i++) {
+      if (this.cascade.get(i).isEmpty()) {
+        noOfEmptyCascadePiles += 1;
+      }
+    }
+    return noOfEmptyCascadePiles;
+  }
+
   private boolean checkBuild(List<Card> cardList) {
-    for (int i = 0 ; i < cardList.size(); i++) {
-      if (i != cardList.size()) {
+    for (int i = 0; i < cardList.size(); i++) {
+      if (i != (cardList.size() - 1)) {
         Card c1 = cardList.get(i);
-        Card c2 = cardList.get(i+1);
+        Card c2 = cardList.get(i + 1);
         if (!(((c2.getNumber() - 1) == c1.getNumber()) && checkAlternateSuit(c1, c2))) {
           return false;
         }
